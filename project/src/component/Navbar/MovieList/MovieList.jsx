0dbf3208ade7ace1699da7a8877846d736220a86
Sonam Gehlod <sonammalviya37@gmail.com>
2025-10-03 11:40:32 +0530

    Add useEffect to fetch popular movies in MovieList component

--- project/src/component/Navbar/MovieList/MovieList.jsx
+++ project/src/component/Navbar/MovieList/MovieList.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './MovieList.css';
 import fire from '../../../assets/fire.png';
 import MovieCard from './MovieCard';
 
 const MovieList = () => {
+    useEffect(() => {
+        await fetch('https://api.themoviedb.org/3/movie/popular?api_key=183928bab7fc630ed0449e4f66ec21bd')
+      
+    }, []);
+    const fetchMovies = async () => {
   return (
     <section className='movie_list'>
         <header className='align-center movie_list_header'>
